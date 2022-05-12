544b3a712eb3b5502d6b1201a76dcc5328383989
Dodgy_Merchant <malte.boenig@gmail.com>
2022-05-12 15:53:32 +0200

    stuff flags broken

--- js/code.js
+++ js/code.js
@@ -1,5 +1,21 @@
-function filterlisttoggle() {
-	var _el = document.getElementById("filter-list");
+function filterlisttoggle(_id) {
+	// var _el = document.getElementById("filter-list");
+	var _el = document.getElementById(_id);
+	var _style = window.getComputedStyle(_el);
 
-	_el.style.display = _el.style.display === "none" ? "block" : "none";
+	console.log("hello");
+
+	if (_style.display == "none") {
+		_style.display = "block";
+	} else {
+		_style.display = "none";
+	}
+
+	// _el.style.display = _el.style.display === "none" ? "block" : "none";
+}
+
+function getIdStyle(_id) {
+	// returns a CSS Style Declaration, all values are named by html style names
+	var _el = document.getElementById(_id);
+	return getComputedStyle(_el);
 }
--- js/template.js
+++ js/template.js
@@ -1,62 +1,65 @@
+//#region  base setup
 //check for template support
 const supportsTemplate = document.createElement("template").content;
 
 //continue or exit
 if (supportsTemplate) {
-  console.log("Your browser supports template!");
+	console.log("Your browser supports template!");
 } else {
-  console.log("Your browser does NOT support template!!!");
-  alert(
-    "Your browser does not fully support this website!\n My portfolio projects won't be displayed currently."
-  );
-  // throw new Error();
-  stop(); //this is not optimal
+	console.log("Your browser does NOT support template!!!");
+	alert(
+		"Your browser does not fully support this website!\n My portfolio projects won't be displayed currently."
+	);
+	// throw new Error();
+	stop(); //this is not optimal
 }
 
 //setup container for content
 const container = document.getElementById("content-destination");
 const template = document.getElementById("content-template").content;
 
+//#endregion
+
 function ContentCreate() {
-  if (!supportsTemplate) return;
+	if (!supportsTemplate) return;
 
-  var _elements = document.querySelectorAll(".project-data");
-  var _leng = _elements.length;
-  for (var i = 0; i < _leng; i++) {
-    _elements[i].id = "abc-" + i;
-  }
+	var _elements = document.querySelectorAll(".project-data");
+	var _leng = _elements.length;
+	for (var i = 0; i < _leng; i++) {
+		_elements[i].id = "abc-" + i;
+	}
 
-  var _elements = document.querySelectorAll(".project-data");
-  var _leng = _elements.length;
-  for (var i = 0; i < _leng; i++) {
-    _elements[i].id = "abc-" + i;
-  }
+	var _elements = document.querySelectorAll(".project-data");
+	var _leng = _elements.length;
+	for (var i = 0; i < _leng; i++) {
+		_elements[i].id = "abc-" + i;
+	}
 
-  var _new_node = ContentCreateArticle(template); //creates a new article with no info
-  var _new_node = ContentCreateArticle(template); //creates a new article with no info
-  var _new_node = ContentCreateArticle(template); //creates a new article with no info
-  var _new_node = ContentCreateArticle(template); //creates a new article with no info
-  var _new_node = ContentCreateArticle(template); //creates a new article with no info
+	var _new_node = ContentCreateArticle(template); //creates a new article with no info
+	var _new_node = ContentCreateArticle(template); //creates a new article with no info
+	var _new_node = ContentCreateArticle(template); //creates a new article with no info
+	var _new_node = ContentCreateArticle(template); //creates a new article with no info
+	var _new_node = ContentCreateArticle(template); //creates a new article with no info
 
-  // var _text = _new_node.("content-text");
-  // _text.style.color = "blue";
+	// var _text = _new_node.("content-text");
+	// _text.style.color = "blue";
 
-  // _node.nodeType
+	// _node.nodeType
 }
 
 function ContentClear() {
-  var _child = container.firstElementChild;
-  while (_child) {
-    container.removeChild(_child);
-    var _child = container.firstElementChild;
-  }
+	var _child = container.firstElementChild;
+	while (_child) {
+		container.removeChild(_child);
+		var _child = container.firstElementChild;
+	}
 }
 
 function ContentCreateArticle(_node) {
-  // var _newClone = _node.firstElementChild.cloneNode(true);
-  var _newClone = document.importNode(_node, true);
-  container.appendChild(_newClone);
-  return _newClone;
+	// var _newClone = _node.firstElementChild.cloneNode(true);
+	var _newClone = document.importNode(_node, true);
+	container.appendChild(_newClone);
+	return _newClone;
 }
 
 /*
