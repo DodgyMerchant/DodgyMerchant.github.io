834c48d974c04671e408239a88ad79d5a2c71e39
DodgyMerchant <malte.boenig@gmail.com>
2023-02-25 21:47:08 +0100

    node eslint init

--- myJS/MyTags.js
+++ myJS/MyTags.js
@@ -14,36 +14,36 @@
  * @author Dodgy_Merchant <admin@dodgymerchant.dev>
  */
 export default class MyTags {
-	/**
-	 * a map of all behaviors and user description text for them.
-	 * @type {Map<TagBehavior,String>}
-	 */
-	// prettier-ignore
-	static TagBehaviorMap = new Map([
-		["one", "Displays all objects with atleast one matching tags."],
-		["all", "Displays all objects with all their tags matching."],
-		["exact","Displays all objects that have the exact same tags as used in the filter. All, not more or less."],
-	]);
-	/**
-	 * expanded map of all behaviors and user description text for them.
-	 * @type {Map<TagBehavior,String>}
-	 */
-	// prettier-ignore
-	static TagBehaviorMapExt = new Map([
-		["one",       "Displays all objects with atleast one matching tag."],
-		["all",       "Displays all objects that have all their tags in the filter."],
-		["match",     "Displays all objects that have matching and no mismatching tags in the filter."],
-		["complete",  "Displays all objects that have no mismatching tags unless all tags are contained in the filter"],
-		["exact",     "Displays all objects that have the exact same tags as used in the filter. All, not more or less."],
-	]);
+  /**
+   * a map of all behaviors and user description text for them.
+   * @type {Map<TagBehavior,String>}
+   */
+  // prettier-ignore
+  static TagBehaviorMap = new Map([
+        ["one", "Displays all objects with atleast one matching tags."],
+        ["all", "Displays all objects with all their tags matching."],
+        ["exact","Displays all objects that have the exact same tags as used in the filter. All, not more or less."],
+    ]);
+  /**
+   * expanded map of all behaviors and user description text for them.
+   * @type {Map<TagBehavior,String>}
+   */
+  // prettier-ignore
+  static TagBehaviorMapExt = new Map([
+        ["one",       "Displays all objects with atleast one matching tag."],
+        ["all",       "Displays all objects that have all their tags in the filter."],
+        ["match",     "Displays all objects that have matching and no mismatching tags in the filter."],
+        ["complete",  "Displays all objects that have no mismatching tags unless all tags are contained in the filter"],
+        ["exact",     "Displays all objects that have the exact same tags as used in the filter. All, not more or less."],
+    ]);
 
-	/* Filter Logic explainaition
-  //
-  //  complete  = all tags in the target are matched to tags in the filter.
-  //
-  //  match     = all tags in the filter are matched to tags in the target.
-  //
-  //
+  /* Filter Logic explainaition
+    //
+    //  complete  = all tags in the target are matched to tags in the filter.
+    //
+    //  match     = all tags in the filter are matched to tags in the target.
+    //
+    //
 	//        target  filter
 	// "one":       one or more mathcing tags
 	// true   ["a", "x"] ["a", "y"]                       true =  incomplete  mismatch
@@ -64,144 +64,142 @@ export default class MyTags {
 	// false  ["a", "b"] ["a", "c"]                       false = incomplete  mismatch
 	// "exact":     Displays all objects that have the exact same tags as used in the filter. All, not more or less.
 	// ["a", "b", "c"] ["a", "b", "c"]                    true =  complete    match
-  */
+    */
 
-	/* test code
-  // prettier-ignore
-{
-  //one
-  console.log("one: ", MyTags.Compare(["a", "x"], ["a", "y"], "one"), "true");
-  console.log("one: ", MyTags.Compare(["a", "x"], ["c", "y"], "one"), "false");
-  //all
-  console.log("all: ", MyTags.Compare(["a", "b", "c"], ["a", "b", "c", "d", "e"], "all"), "true");
-  console.log("all: ", MyTags.Compare(["a", "b", "c"], ["a", "b", "c"], "all"), "true");
-  console.log("all: ", MyTags.Compare(["a", "b", "c"], ["a", "b"], "all"), "false");
-  //match
-  console.log("match: ", MyTags.Compare(["a", "b"], ["a"], "match"), "true");
-  console.log("match: ", MyTags.Compare(["a", "b"], ["a", "b"], "match"), "true");
-  console.log("match: ", MyTags.Compare(["a", "b"], ["a", "c"], "match"), "false");
-  console.log("match: ", MyTags.Compare(["a", "b"], ["a", "b", "c"], "match"), "false");
-  //complete
-  console.log("complete: ", MyTags.Compare(["a", "b"], ["a"], "complete"), "true");
-  console.log("complete: ", MyTags.Compare(["a", "b"], ["a", "b", "c"], "complete"), "true");
-  console.log("complete: ", MyTags.Compare(["a", "b"], ["a", "b"], "complete"), "true");
-  console.log("complete: ", MyTags.Compare(["a", "b"], ["a", "c"], "complete"), "false");
-  //exact
-  console.log("exact: ", MyTags.Compare(["a", "b", "c"], ["a", "b", "c"], "exact"), "true");
-  console.log("exact: ", MyTags.Compare(["a", "b", "c"], ["a", "b", "x"], "exact"), "false");
+  /* test code
+    // prettier-ignore
+    {
+    //one
+    console.log("one: ", MyTags.Compare(["a", "x"], ["a", "y"], "one"), "true");
+    console.log("one: ", MyTags.Compare(["a", "x"], ["c", "y"], "one"), "false");
+    //all
+    console.log("all: ", MyTags.Compare(["a", "b", "c"], ["a", "b", "c", "d", "e"], "all"), "true");
+    console.log("all: ", MyTags.Compare(["a", "b", "c"], ["a", "b", "c"], "all"), "true");
+    console.log("all: ", MyTags.Compare(["a", "b", "c"], ["a", "b"], "all"), "false");
+    //match
+    console.log("match: ", MyTags.Compare(["a", "b"], ["a"], "match"), "true");
+    console.log("match: ", MyTags.Compare(["a", "b"], ["a", "b"], "match"), "true");
+    console.log("match: ", MyTags.Compare(["a", "b"], ["a", "c"], "match"), "false");
+    console.log("match: ", MyTags.Compare(["a", "b"], ["a", "b", "c"], "match"), "false");
+    //complete
+    console.log("complete: ", MyTags.Compare(["a", "b"], ["a"], "complete"), "true");
+    console.log("complete: ", MyTags.Compare(["a", "b"], ["a", "b", "c"], "complete"), "true");
+    console.log("complete: ", MyTags.Compare(["a", "b"], ["a", "b"], "complete"), "true");
+    console.log("complete: ", MyTags.Compare(["a", "b"], ["a", "c"], "complete"), "false");
+    //exact
+    console.log("exact: ", MyTags.Compare(["a", "b", "c"], ["a", "b", "c"], "exact"), "true");
+    console.log("exact: ", MyTags.Compare(["a", "b", "c"], ["a", "b", "x"], "exact"), "false");
+    }
+    */
+
+  /**
+   * checks if target tags correspont to the filter tags using given behavior.
+   * If target tag list is empty will only be drawn if the filter tags are also empty.
+   * @param {string[]=} targetTags List of Tags belonging to the target.
+   * @param {string[]=} filterTags List of Tags to filter by. If no tags are given it will always return true.
+   * @param {TagBehavior} behavior "one" or more. "all" or more. "exact"ly the items, no more or less.
+   * @returns {boolean}
+   */
+  static Compare(targetTags, filterTags, behavior) {
+    let o, f, Otag, Ftag;
+
+    let trgEmpty = targetTags.length == 0;
+    let tagEmpty = !filterTags || filterTags.length == 0;
+
+    if (!trgEmpty && !tagEmpty) {
+      switch (behavior) {
+        case "one":
+          //look for one match
+
+          for (f = 0; f < filterTags.length; f++) {
+            Ftag = filterTags[f];
+            for (o = 0; o < targetTags.length; o++) {
+              Otag = targetTags[o];
+
+              if (Otag == Ftag) {
+                return true;
+              }
+            }
+          }
+          return false;
+        case "all":
+          //all target tags must be in the filter tags
+          // for (let i = 0; i < filterTags.length; i++) {
+          // 	if (!targetTags.includes(filterTags[i])) {
+          // 		return false;
+          // 	}
+          // }
+
+          return this._cc(targetTags, filterTags);
+        case "match":
+          return this._cm(targetTags, filterTags);
+        case "complete":
+          if (
+            !this._cc(targetTags, filterTags) &&
+            !this._cm(targetTags, filterTags)
+          )
+            return false;
+          return true;
+        case "exact":
+          //#region old
+          // //looks for the exact same tags
+          // feed = false;
+
+          // //early exit if tag number isnt equal
+          // if (targetTags.length != filterTags.length) return false;
+
+          // for (o = 0; o < targetTags.length; o++) {
+          // 	Otag = targetTags[o];
+          // 	for (f = 0; f < filterTags.length; f++) {
+          // 		Ftag = filterTags[f];
+
+          // 		if (Ftag == Otag) {
+          // 			feed = true;
+          // 			break;
+          // 		}
+          // 	}
+
+          // 	if (!feed) {
+          // 		return false;
+          // 	} else {
+          // 		feed = false;
+          // 	}
+          // }
+
+          // return true;
+
+          //#endregion old
+
+          return (
+            this._cc(targetTags, filterTags) && this._cm(targetTags, filterTags)
+          );
+      }
+    } else {
+      //if object tag list empty only draw if filter list is also empty
+      if (trgEmpty) {
+        if (tagEmpty) return true;
+        else return false;
+      }
+
+      return true;
+    }
   }
-*/
 
-	/**
-	 * checks if target tags correspont to the filter tags using given behavior.
-	 * If target tag list is empty will only be drawn if the filter tags are also empty.
-	 * @param {string[]=} targetTags List of Tags belonging to the target.
-	 * @param {string[]=} filterTags List of Tags to filter by. If no tags are given it will always return true.
-	 * @param {TagBehavior} behavior "one" or more. "all" or more. "exact"ly the items, no more or less.
-	 * @returns {boolean}
-	 */
-	static Compare(targetTags, filterTags, behavior) {
-		let o, f, Otag, Ftag;
-		let feed;
+  static _cc(t, f) {
+    // check for completion
 
-		let trgEmpty = targetTags.length == 0;
-		let tagEmpty = !filterTags || filterTags.length == 0;
+    if (t.length > f.length) return false;
 
-		if (!trgEmpty && !tagEmpty) {
-			switch (behavior) {
-				case "one":
-					//look for one match
+    for (let i = 0; i < t.length; i++) {
+      if (!f.includes(t[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+  static _cm(t, f) {
+    // check for matching
 
-					for (f = 0; f < filterTags.length; f++) {
-						Ftag = filterTags[f];
-						for (o = 0; o < targetTags.length; o++) {
-							Otag = targetTags[o];
-
-							if (Otag == Ftag) {
-								return true;
-							}
-						}
-					}
-					return false;
-				case "all":
-					//all target tags must be in the filter tags
-					// for (let i = 0; i < filterTags.length; i++) {
-					// 	if (!targetTags.includes(filterTags[i])) {
-					// 		return false;
-					// 	}
-					// }
-
-					return this._cc(targetTags, filterTags);
-
-				case "match":
-					return this._cm(targetTags, filterTags);
-				case "complete":
-					if (
-						!this._cc(targetTags, filterTags) &&
-						!this._cm(targetTags, filterTags)
-					)
-						return false;
-					return true;
-				case "exact":
-					//#region old
-					// //looks for the exact same tags
-					// feed = false;
-
-					// //early exit if tag number isnt equal
-					// if (targetTags.length != filterTags.length) return false;
-
-					// for (o = 0; o < targetTags.length; o++) {
-					// 	Otag = targetTags[o];
-					// 	for (f = 0; f < filterTags.length; f++) {
-					// 		Ftag = filterTags[f];
-
-					// 		if (Ftag == Otag) {
-					// 			feed = true;
-					// 			break;
-					// 		}
-					// 	}
-
-					// 	if (!feed) {
-					// 		return false;
-					// 	} else {
-					// 		feed = false;
-					// 	}
-					// }
-
-					// return true;
-
-					//#endregion old
-
-					return (
-						this._cc(targetTags, filterTags) && this._cm(targetTags, filterTags)
-					);
-			}
-		} else {
-			//if object tag list empty only draw if filter list is also empty
-			if (trgEmpty) {
-				if (tagEmpty) return true;
-				else return false;
-			}
-
-			return true;
-		}
-	}
-
-	static _cc(t, f) {
-		// check for completion
-
-		if (t.length > f.length) return false;
-
-		for (let i = 0; i < t.length; i++) {
-			if (!f.includes(t[i])) {
-				return false;
-			}
-		}
-		return true;
-	}
-	static _cm(t, f) {
-		// check for matching
-
-		return this._cc(f, t);
-	}
+    return this._cc(f, t);
+  }
 }
--- myJS/MyTemplate.js
+++ myJS/MyTemplate.js
@@ -1,13 +1,12 @@
 //check for Template support
 const supportsTemplate = new Boolean(
-	document.createElement("Template").content
+  document.createElement("Template").content
 );
 //continue or exit
 if (supportsTemplate) {
-	console.log("Your browser supports Template!");
+  console.log("Your browser supports Template!");
 } else {
-	console.error("Your browser does NOT support Template!!!");
-	if (alertMsg) alert(errorMsg);
+  console.error("Your browser does NOT support Template!!!");
 }
 
 /**
@@ -17,53 +16,53 @@ if (supportsTemplate) {
  * @author Dodgy_Merchant <admin@dodgymerchant.dev>
  */
 export default class MyTemplate {
-	/**
-	 *  only creates element from template
-	 * @param {HTMLTemplateElement} template
-	 * @returns {HTMLElement} appended child of template
-	 */
-	static create(template) {
-		if (!supportsTemplate) return;
+  /**
+   *  only creates element from template
+   * @param {HTMLTemplateElement} template
+   * @returns {HTMLElement} appended child of template
+   */
+  static create(template) {
+    if (!supportsTemplate) return;
 
-		return document.importNode(template.content, true);
-	}
+    return document.importNode(template.content, true);
+  }
 
-	/**
-	 * clers of all child elements
-	 * @param {HTMLElement} parEl
-	 */
-	static clearAll(parEl) {
-		var _child = parEl.firstElementChild;
-		while (_child) {
-			parEl.removeChild(_child);
-			var _child = parEl.firstElementChild;
-		}
-	}
+  /**
+   * clers of all child elements
+   * @param {HTMLElement} parEl
+   */
+  static clearAll(parEl) {
+    let _child = parEl.firstElementChild;
+    while (_child) {
+      parEl.removeChild(_child);
+      _child = parEl.firstElementChild;
+    }
+  }
 
-	static addChild(child, target) {
-		return target.appendChild(child);
-	}
-	/**
-	 * adds all template children to target element and returns a list of all added elelemnts.
-	 * @param {HTMLTemplateElement} template
-	 * @param {HTMLElement} target
-	 * @returns {Node[]}
-	 */
-	static addTemplate(template, target) {
-		let list = [];
-		let temp = this.create(template);
-		do {
-			list.push(this.addChild(temp.firstElementChild, target));
-		} while (temp.childElementCount > 0);
+  static addChild(child, target) {
+    return target.appendChild(child);
+  }
+  /**
+   * adds all template children to target element and returns a list of all added elelemnts.
+   * @param {HTMLTemplateElement} template
+   * @param {HTMLElement} target
+   * @returns {Node[]}
+   */
+  static addTemplate(template, target) {
+    let list = [];
+    let temp = this.create(template);
+    do {
+      list.push(this.addChild(temp.firstElementChild, target));
+    } while (temp.childElementCount > 0);
 
-		return list;
-	}
+    return list;
+  }
 
-	/**
-	 * returns if the browser supports template.
-	 * @returns {Boolean}
-	 */
-	static supports() {
-		return supportsTemplate;
-	}
+  /**
+   * returns if the browser supports template.
+   * @returns {Boolean}
+   */
+  static supports() {
+    return supportsTemplate;
+  }
 }
